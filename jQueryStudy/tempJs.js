3a1bacf836dd56901613aff2c8d4128c34c6aa4a
hoggenw <253192463@qq.com>
2017-08-06 21:29:57 +0800

    no message

---
+++ jQueryStudy/tempJs.js
@@ -0,0 +1,54 @@
+/**
+ * Created by wangliugen on 2017/8/3.
+ */
+$(document).ready(function () {
+    $("button#buttton1").click(function () {
+        $("#test").hide();
+    })
+    $("button#buttton2").click(function () {
+        $(".test").hide();
+    })
+    $("p.test").click(function () {
+        $(this).toggle();
+    })
+    $("p#test").click(function () {
+        $(this).hide();
+    })
+    $("button#show").click(function () {
+        $("p").show();
+    })
+    // $("button#fadein").click(function () {
+    //     $("#div1").fadeIn();
+    //     $("#div2").fadeIn("slow");
+    //     $("#div3").fadeIn(3000);
+    // })
+    $("button#fadein").click(function () {
+        $("#div1").fadeToggle();
+        $("#div2").fadeToggle("slow");
+        $("#div3").fadeToggle(3000);
+    })
+    $("button#fadeout").click(function () {
+        $("#div1").fadeOut();
+        $("#div2").fadeOut("slow");
+        $("#div3").fadeOut(3000);
+    })
+    $("button#fadeTo").click(function () {
+        $("#div1").fadeTo("slow",0.15);
+        $("#div2").fadeTo("slow",0.4);
+        $("#div3").fadeTo("slow",0.7);
+    })
+
+    $("p#double").dblclick(function () {
+        $(this).hide();
+    })
+    $("p#double").focus(function () {
+        $(this).css("background-color","#cccccc");
+    })
+
+    // $("#double").mouseenter(function () {
+    //     alert("进入元素")
+    // })
+    // $("#double").mouseleave(function () {
+    //     alert("离开元素")
+    // })
+})(jquery)
