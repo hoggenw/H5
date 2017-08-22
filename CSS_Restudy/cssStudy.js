3ef13ab5ef2a26dd4d819353f0051fef285cdcef
hoggenw <253192463@qq.com>
2017-08-22 09:53:38 +0800

    H5 study

---
+++ CSS_Restudy/cssStudy.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wangliugen on 2017/8/21.
+ */
+$(document).ready(function () {
+
+    $("#action").click(function () {
+        $("#imageDiv").css("visibility" , "hidden");
+    });
+});
