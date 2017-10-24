6b9058e91b7d582173f5b29d7af2de4a2333cde7
hoggenw <253192463@qq.com>
2017-10-24 10:11:50 +0800

    js go on

---
+++ vedeoJS/js1.js
@@ -0,0 +1,11 @@
+
+
+
+window.onload = function () {
+    let topBannerClose = document.getElementById("topBananerClose");
+    let top_Banner = document.getElementById("top_Banner");
+    topBannerClose.onclick = function () {
+        top_Banner.style.display = "none";
+
+    };
+};
