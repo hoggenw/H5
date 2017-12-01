5aa78c8f11c04cef190f8bed3261835698096baa
hoggenw <253192463@qq.com>
2017-12-01 10:10:27 +0800

    no message

--- vedeoJS/demoMouse/mouseJS.js
+++ vedeoJS/demoMouse/mouseJS.js
@@ -10,12 +10,27 @@
         }
 
     }
+
      changedBackGroudImage("imageView01","url(../images/01big.jpg)");
      changedBackGroudImage("imageView02","url(../images/02big.jpg)");
      changedBackGroudImage("imageView03","url(../images/03big.jpg)");
      changedBackGroudImage("imageView04","url(../images/04big.jpg)");
      changedBackGroudImage("imageView05","url(../images/05big.jpg)");
 
+    function getElement(elementID) {
+        return document.getElementById(elementID);
 
+    }
+
+    getElement("findButton").onclick = function () {
+        if(getElement("input2").value === "平静"){
+            alert("中奖");
+        }else  {
+            alert("没有");
+        }
+
+
+        console.log("findButton");
+    };
 
 };
