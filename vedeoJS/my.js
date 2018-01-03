fc90c4e72617d27ebe9f791cbe031df2f18483d5
hoggenw <253192463@qq.com>
2018-01-03 10:06:17 +0800

    no message

--- vedeoJS/my.js
+++ vedeoJS/my.js
@@ -25,3 +25,25 @@ function scroll() {
         top: document.body.scrollTop
     }
 }
+
+function client() {
+    if(window.innerWidth != null)  // ie9 +  最新浏览器
+    {
+        return {
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+    }
+    else if(document.compatMode === "CSS1Compat")  // 标准浏览器
+    {
+        return {
+            width: document.documentElement.clientWidth,
+            height: document.documentElement.clientHeight
+        }
+    }
+    return {   // 怪异浏览器
+        width: document.body.clientWidth,
+        height: document.body.clientHeight
+
+    }
+}
